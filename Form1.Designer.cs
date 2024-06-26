0978958af1c2923b59a5e1ef4c47f1424c9aa259
FrancoSchmid <francoschmid9@gmail.com>
2024-06-26 20:27:46 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace pryParcialLog2
+{
+    partial class frm_Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Listar = new System.Windows.Forms.Button();
+            this.btn_InfCliente = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Listar
+            // 
+            this.btn_Listar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_Listar.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Listar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Listar.Location = new System.Drawing.Point(12, 12);
+            this.btn_Listar.Name = "btn_Listar";
+            this.btn_Listar.Size = new System.Drawing.Size(384, 112);
+            this.btn_Listar.TabIndex = 0;
+            this.btn_Listar.Text = "Listar cuentas";
+            this.btn_Listar.UseVisualStyleBackColor = false;
+            // 
+            // btn_InfCliente
+            // 
+            this.btn_InfCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_InfCliente.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_InfCliente.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_InfCliente.Location = new System.Drawing.Point(12, 141);
+            this.btn_InfCliente.Name = "btn_InfCliente";
+            this.btn_InfCliente.Size = new System.Drawing.Size(384, 112);
+            this.btn_InfCliente.TabIndex = 1;
+            this.btn_InfCliente.Text = "Informes por cliente";
+            this.btn_InfCliente.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.Location = new System.Drawing.Point(12, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(384, 112);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Informe por tipo de transacción";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // frm_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(408, 397);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_InfCliente);
+            this.Controls.Add(this.btn_Listar);
+            this.Name = "frm_Menu";
+            this.Text = "Banco del IES";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Listar;
+        private System.Windows.Forms.Button btn_InfCliente;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
